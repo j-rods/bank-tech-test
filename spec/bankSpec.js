385c713f499f9c30f0870e82d208ef4b95fae799
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-15 11:19:51 +0100

    withdraw an amount

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -3,7 +3,14 @@ describe ('user', function() {
   it('should be able to make a deposit of a certain amount', function() {
     var account = new BankAccount();
     account.deposit(20);
-    expect(account.transactions[account.transactions.length-1]).toEqual(20);
+    expect(account.transactions[account.transactions.length-1].balance).toEqual(20);
+  });
+
+  it('should be able to make a withdrawal of a certain amount', function() {
+    var account = new BankAccount();
+    account.deposit(20);
+    account.withdraw(5);
+    expect(account.transactions[account.transactions.length-1].balance).toEqual(15);
   });
 
 });
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -4,5 +4,12 @@ function BankAccount() {
 
 BankAccount.prototype.deposit = function(amount) {
   //push amount into array
-  this.transactions.push(amount);
+  this.transactions.push({balance: amount});
+}
+
+BankAccount.prototype.withdraw = function(amount) {
+  //pop amount from array
+  var total = 0;
+  total = this.transactions[this.transactions.length-1].balance - amount;
+  this.transactions.push({balance: total});
 }
