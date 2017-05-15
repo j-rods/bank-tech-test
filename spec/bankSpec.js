00136dd863027a9f3352e54d5359b9581be03499
Jessica Rodriguez <jrrodriguez84@gmail.com>
2017-05-15 11:23:04 +0100

    refactor var scope

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,13 +1,21 @@
+(function () {
+  'use strict';
+}());
+
+var account;
+
+beforeEach(function() {
+  account = new BankAccount();
+});
+
 describe ('user', function() {
 
   it('should be able to make a deposit of a certain amount', function() {
-    var account = new BankAccount();
     account.deposit(20);
     expect(account.transactions[account.transactions.length-1].balance).toEqual(20);
   });
 
   it('should be able to make a withdrawal of a certain amount', function() {
-    var account = new BankAccount();
     account.deposit(20);
     account.withdraw(5);
     expect(account.transactions[account.transactions.length-1].balance).toEqual(15);
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,3 +1,7 @@
+(function () {
+  'use strict';
+}());
+
 function BankAccount() {
   this.transactions = []
 }
